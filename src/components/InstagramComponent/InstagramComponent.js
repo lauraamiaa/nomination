926d4671046d5550583c4dff5b15c2b74c470eca
Laura Miatkowski <miatkowski.laura@gmail.com>
2023-04-26 09:38:50 +0200

    instagram component

---
+++ src/components/InstagramComponent/InstagramComponent.js
@@ -0,0 +1,19 @@
+function InstagramComponent() {
+  return (
+    <div className="c-instagram__outer-wrapper">
+      <div className="c-instagram__inner-wrapper">
+        <div className="c-instagram__header">
+          <h2>JOIN THE FAM @NOMINATIONITALY</h2>
+        </div>
+        <div className="c-instagram__grid-container">
+          <div className="c-instagram__grid-column"></div>
+          <div className="c-instagram__grid-column"></div>
+          <div className="c-instagram__grid-column"></div>
+          <div className="c-instagram__grid-column"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InstagramComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import "../styles/main.scss";
 
 import ProductComponent from "../components/ProductComponent/ProductComponent";
 import PromoSwiper from "../components/PromoSwiper/PromoSwiper";
+import InstagramComponent from "../components/InstagramComponent/InstagramComponent";
 
 const Home = () => {
   return (
     <div className="p-homepage">
       <ProductComponent />
       <PromoSwiper />
+      <InstagramComponent />
     </div>
   );
 };
