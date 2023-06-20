137620ed3c37368c80bdd3a164826d15fc0ef25e
Laura Miatkowski <miatkowski.laura@gmail.com>
2023-06-20 20:06:51 +0200

    page adjustments

--- src/components/PromoSwiper/PromoSwiper.js
+++ src/components/PromoSwiper/PromoSwiper.js
@@ -7,17 +7,7 @@ function PromoSwiper() {
     <div className="c-promo-swiper__outer-wrapper">
       <div className="c-promo-swiper__inner-wrapper">
         <div className="c-promo-swiper__header">
-          <img
-            class="c-promo-swiper__heart-emoji"
-            src={`${process.env.PUBLIC_URL}/heart.png`}
-            alt="heart"
-          />
           <h2>DESIGNED BY YOU</h2>
-          <img
-            class="c-promo-swiper__heart-emoji"
-            src={`${process.env.PUBLIC_URL}/heart.png`}
-            alt="heart"
-          />
         </div>
         <Swiper
           className="c-promo-swiper__collection-swiper"
@@ -34,26 +24,66 @@ function PromoSwiper() {
           spaceBetween={5}
           slidesPerView={11}
         >
-          <SwiperSlide className="charm-1"></SwiperSlide>
-          <SwiperSlide className="charm-2"></SwiperSlide>
-          <SwiperSlide className="charm-3"></SwiperSlide>
-          <SwiperSlide className="charm-4"></SwiperSlide>
-          <SwiperSlide className="charm-5"></SwiperSlide>
-          <SwiperSlide className="charm-6"></SwiperSlide>
-          <SwiperSlide className="charm-7"></SwiperSlide>
-          <SwiperSlide className="charm-8"></SwiperSlide>
-          <SwiperSlide className="charm-9"></SwiperSlide>
-          <SwiperSlide className="charm-10"></SwiperSlide>
-          <SwiperSlide className="charm-11"></SwiperSlide>
-          <SwiperSlide className="charm-12"></SwiperSlide>
-          <SwiperSlide className="charm-13"></SwiperSlide>
-          <SwiperSlide className="charm-14"></SwiperSlide>
-          <SwiperSlide className="charm-15"></SwiperSlide>
-          <SwiperSlide className="charm-16"></SwiperSlide>
-          <SwiperSlide className="charm-17"></SwiperSlide>
-          <SwiperSlide className="charm-18"></SwiperSlide>
-          <SwiperSlide className="charm-19"></SwiperSlide>
-          <SwiperSlide className="charm-20"></SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-1"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-2"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-3"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-4"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-5"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-6"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-7"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-8"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-9"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-10"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-11"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-12"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-13"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-14"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-15"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-16"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-17"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-18"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-19"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="charm-20"></div>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
