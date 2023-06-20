da057105819c02ba799ef69af363e99032c7ae44
Laura Miatkowski <miatkowski.laura@gmail.com>
2023-06-20 20:45:11 +0200

    update

--- src/components/InstagramComponent/InstagramComponent.js
+++ src/components/InstagramComponent/InstagramComponent.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 function InstagramComponent() {
   return (
     <div className="c-instagram__outer-wrapper">
@@ -5,52 +9,54 @@ function InstagramComponent() {
         <div className="c-instagram__header">
           <h2>JOIN THE FAM @NOMINATIONITALY</h2>
         </div>
-        <div className="c-instagram__grid-container">
-          <div className="c-instagram__first-row">
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-1.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-2.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-3.png`}
-            />
-          </div>
-          <div className="c-instagram__second-row">
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/instagram-2.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/instagram-3.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/instagram-1.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-3.png`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-8.jpeg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/instagram-5.jpg`}
-            />
-            <img
-              alt="instagram"
-              src={`${process.env.PUBLIC_URL}/insta-10.jpeg`}
-            />
-          </div>
-        </div>
+        <Swiper
+          className="c-instagram__swiper"
+          spaceBetween={5}
+          slidesPerView={4}
+        >
+          <SwiperSlide>
+            <div className="insta-1"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-2"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-3"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-4"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-5"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-6"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-7"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-8"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-9"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-10"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-11"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-12"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-13"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="insta-14"></div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
